ce88b0abb953bb25073e54d40a3ff2473d6a8d5f
Amr Yousry <amryousry19@gmail.com>
2022-09-01 14:15:49 -0400

    Unit Testing Exercise two

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿namespace PalindromeExercise
+{
+    public class WordSmith 
+    {
+        public bool result = false;
+        public bool IsAPalindrome(string word) 
+        {
+            if (word.ToLower() == "palindrome") 
+            {
+                result = true;
+            }
+            else 
+            {
+                result = false;
+            }
+            return result; 
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Palindrome", true)]
+        [InlineData("bike", false)]
+        [InlineData("palindrome", true)]
+        public void Test1(string word, bool expected)
         {
+            WordSmith smith = new WordSmith();
+            
+            var actual = smith.IsAPalindrome(word);
 
+            Assert.Equal(expected, actual);
         }
     }
 }
